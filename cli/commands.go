8ed0568a94284893bf3c09450aa69da9f8f72fdd
Spencer Herzberg <spencer.herzberg@gmail.com>
2015-05-08 14:45:18 -0500

    marathon auth
    * policies = appId
    * checks auth against marathon
    * must auth within 5 seconds of startedAt

--- cli/commands.go
+++ cli/commands.go
@@ -13,6 +13,7 @@ import (
 	credCert "github.com/hashicorp/vault/builtin/credential/cert"
 	credGitHub "github.com/hashicorp/vault/builtin/credential/github"
 	credLdap "github.com/hashicorp/vault/builtin/credential/ldap"
+	credMarathon "github.com/hashicorp/vault/builtin/credential/marathon"
 	credUserpass "github.com/hashicorp/vault/builtin/credential/userpass"
 
 	"github.com/hashicorp/vault/builtin/logical/aws"
@@ -66,6 +67,7 @@ func Commands(metaPtr *command.Meta) map[string]cli.CommandFactory {
 					"github":   credGitHub.Factory,
 					"userpass": credUserpass.Factory,
 					"ldap":     credLdap.Factory,
+					"marathon": credMarathon.Factory,
 				},
 				LogicalBackends: map[string]logical.Factory{
 					"aws":        aws.Factory,
@@ -101,6 +103,7 @@ func Commands(metaPtr *command.Meta) map[string]cli.CommandFactory {
 					"userpass": &credUserpass.CLIHandler{},
 					"ldap":     &credLdap.CLIHandler{},
 					"cert":     &credCert.CLIHandler{},
+					"marathon": &credMarathon.CLIHandler{},
 				},
 			}, nil
 		},
